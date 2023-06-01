0d343d51e2af3884b1a2fd3538a760f4705a9cd6
Lara Magalhães Tagliani <laragliani.maga27@gmail.com>
2023-06-01 03:06:29 -0300

    atualização 01/06

--- javascript/script.js
+++ javascript/script.js
@@ -1,105 +1,107 @@
-// Web Design
+// Web Design Chart
+//Habilidades grafico
 var webDesignChart = new Chart(document.getElementById('web-design-chart'), {
     type: 'doughnut',
     data: {
+      labels: ['Web Design'],
       datasets: [{
-        data: [90, 10],
-        backgroundColor: ['#fdb157', '#eaeaea'],
-        borderWidth: 0
+        data: [90],
+        backgroundColor: ['#fdb157']
       }]
     },
     options: {
-      cutout: '80%',
-      responsive: true,
-      maintainAspectRatio: false,
-      tooltips: {
-        enabled: false
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      plugins: {
+        labels: {
+          render: 'percentage',
+          fontColor: '#fff',
+          fontSize: 12,
+          precision: 0
+        }
       }
     }
   });
-  
-  // HTML5
+
   var html5Chart = new Chart(document.getElementById('html5-chart'), {
     type: 'doughnut',
     data: {
+      labels: ['HTML5'],
       datasets: [{
-        data: [80, 20],
-        backgroundColor: ['#9473e6', '#eaeaea'],
-        borderWidth: 0
+        data: [80],
+        backgroundColor: ['#9473e6']
       }]
     },
     options: {
-      cutout: '80%',
-      responsive: true,
-      maintainAspectRatio: false,
-      tooltips: {
-        enabled: false
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      plugins: {
+        labels: {
+          render: 'percentage',
+          fontColor: '#fff',
+          fontSize: 12,
+          precision: 0
+        }
       }
     }
   });
-  
-  // CSS3
+
   var css3Chart = new Chart(document.getElementById('css3-chart'), {
     type: 'doughnut',
     data: {
+      labels: ['CSS3'],
       datasets: [{
-        data: [85, 15],
-        backgroundColor: ['#bdecf6', '#eaeaea'],
-        borderWidth: 0
+        data: [85],
+        backgroundColor: ['#bdecf6']
       }]
     },
     options: {
-      cutout: '80%',
-      responsive: true,
-      maintainAspectRatio: false,
-      tooltips: {
-        enabled: false
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      plugins: {
+        labels: {
+          render: 'percentage',
+          fontColor: '#fff',
+          fontSize: 12,
+          precision: 0
+        }
       }
     }
   });
-  
-  // JavaScript
+
   var javascriptChart = new Chart(document.getElementById('javascript-chart'), {
     type: 'doughnut',
     data: {
+      labels: ['JavaScript'],
       datasets: [{
-        data: [50, 50],
-        backgroundColor: ['#ffbcaa', '#eaeaea'],
-        borderWidth: 0
+        data: [60],
+        backgroundColor: ['#ffbcaa']
       }]
     },
     options: {
-      cutout: '80%',
-      responsive: true,
-      maintainAspectRatio: false,
-      tooltips: {
-        enabled: false
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      plugins: {
+        labels: {
+          render: 'percentage',
+          fontColor: '#fff',
+          fontSize: 12,
+          precision: 0
+        }
       }
     }
   });
+//  fim do grafico
 
-
-  $(document).ready(function() {
-    // Web Design
-    var webDesignChart = new Chart(document.getElementById('web-design-chart'), {
-      // ... chart configuration code ...
-    });
-  
-    // HTML5
-    var html5Chart = new Chart(document.getElementById('html5-chart'), {
-      // ... chart configuration code ...
-    });
-  
-    // CSS3
-    var css3Chart = new Chart(document.getElementById('css3-chart'), {
-      // ... chart configuration code ...
-    });
-  
-    // JavaScript
-    var javascriptChart = new Chart(document.getElementById('javascript-chart'), {
-      // ... chart configuration code ...
-    });
   
     // Initialize Carousel
     $('.carousel').carousel();
-  });
+  
